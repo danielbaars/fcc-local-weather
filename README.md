34eabe817683690ca21f0c9a10fd2343e8afac05
Daniel Baars <info@danielbaars.nl>
2017-05-24 11:18:43 +0200

    Big existing files upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# fcc-local-weather
+# Show the Local Weather (freeCodeCamp)
+A webpage that shows the local weather, built as part of [freeCodeCamp](https://www.freecodecamp.com)'s 'Intermediate Front End Development Projects', using [Pug](https://github.com/pugjs/pug), [SCSS](https://github.com/sass/libsass) and [Bootstrap 4](https://github.com/twbs/bootstrap).
+
+![Screenshot of project](https://danielbaars.github.io/fcc-local-weather/screenshot.png)
+
+[View this project live](https://danielbaars.github.io/fcc-local-weather/)
---
+++ index.js
@@ -0,0 +1,66 @@
+var latitude, longitude;
+
+$(document).ready(function(){
+
+	$.getJSON('http://freegeoip.net/json/', function(data) {
+
+		latitude = data.latitude;
+		longitude = data.longitude;
+		onPositionReady();
+
+	});
+
+});
+
+
+
+function onPositionReady() {
+
+	function showPos() {
+
+		$.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=b963fa5ee1b5df1a5e5b13e30bb9da05', function(data) {
+
+			var name = data.name;
+			var country = data.sys.country;
+			var tempC = Math.floor(data.main.temp - 273.15);
+			var tempF = Math.floor((( data.main.temp - 273.15) * 9/5) + 32);
+			var icon = data.weather[0].icon;
+
+			var html = name + ', ' + country + ' --- ' + tempC + '&#8451;, ' + tempF + '&#8457; <img src="http://openweathermap.org/img/w/' + icon + '.png">';
+
+			$(".weather__city").text(name);
+			$(".weather__temperature--celsius").html(tempC + ' &#8451;');
+			$(".weather__temperature--fahrenheit").html(tempF + ' &#8457;');
+			$(".weather__icon").html('<img src="http://openweathermap.org/img/w/' + icon + '.png">');
+
+		});
+
+	}	
+
+	showPos();
+
+	$(".weather__units").on("click", function(){
+
+		$(".weather__temperature--celsius").toggleClass("hide");
+		$(".weather__temperature--fahrenheit").toggleClass("hide");
+		$(".weather__units--celsius").toggleClass("hide");
+		$(".weather__units--fahrenheit").toggleClass("hide");		
+		
+	});	
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
